a25a628a4b9aecb69105fece8ba0a3bf7355dcee
Jezuz <Jezuz@DESKTOP-RN4DP0B>
2024-02-26 21:25:23 -0600

    Agregar archivos de proyecto.

---
+++ Formulas de geometria/Area.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formulas_de_geometria
+{
+    internal class Area
+    {
+        public  double forlmula() 
+        {
+            double altura = 23;
+            double Base = 12;
+            double Entre = 2;
+            
+            double resul = altura * Base / Entre;
+            return resul;
+        }
+
+        public double forluma1(double altura,double Base , double Entre)
+        {
+            double resul = altura *  Base / Entre;
+            return resul;
+        }
+
+        private void formulas2() 
+        {
+            double altura = 18;
+            double Base = 20;
+            double Entre = 2;
+
+            double resul = altura * Base / Entre;
+            Console.WriteLine("el resultado es" + resul);
+        }
+
+        private double altura;
+        private double entre;
+        private double bASE;
+
+        public double Altura { get => altura; set => altura = value; }
+        public double Entre { get => entre; set => entre = value; }
+        public double BASE { get => bASE; set => bASE = value; }
+
+        public void formulas(double altura,double entre,double bASE) 
+        { 
+            this.altura = altura;
+            this.entre = entre;
+            this.bASE = bASE;
+        }
+    }
+   
+}
---
+++ Formulas de geometria/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Formulas_de_geometria
+{
+    internal class program
+    {
+        public static void Main(string[] args)
+        {
+            Area area = new Area();
+
+            double resultado1 = area.forlmula();
+            Console.WriteLine("Resultado de la fórmula sin parámetros: " + resultado1);
+
+            double resultado2 = area.forluma1(23, 12, 2);
+            Console.WriteLine("Resultado de la fórmula con parámetros: " + resultado2);
+
+            area.formulas(18, 24, 2);
+            Console.WriteLine("resultado sin lectura" + area.Altura * area.BASE / area.Entre);
+         
+
+        }
+    }
+}
